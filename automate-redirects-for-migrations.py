78e3a3f0d2f3bdaaf1b55e3eaf4b0cfcc8f7b455
Daniel Emery <86828899+perth-seo-agency@users.noreply.github.com>
2023-11-21 00:41:01 +0800

    Update automate-redirects-for-migrations.py

--- automate-redirects-for-migrations.py
+++ automate-redirects-for-migrations.py
@@ -1,120 +1,60 @@
 # redirect_matchmaker_streamlit.py
-
 import streamlit as st
 import pandas as pd
 from sentence_transformers import SentenceTransformer
-import numpy as np
-import faiss
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Title
-st.title("Automated Redirect Matchmaker v3.0.3")
+st.title("Automated Redirect Matchmaker v3.0.4")
+
+# Initialize Sentence Transformer Model
+model_name = 'all-MiniLM-L6-v2'  # Example model, replace with the model you are using
+model = SentenceTransformer(model_name)
+
+@st.cache
+def calculate_similarity(text1, text2):
+    embeddings1 = model.encode([text1], convert_to_tensor=True)
+    embeddings2 = model.encode([text2], convert_to_tensor=True)
+    cosine_scores = cosine_similarity(embeddings1, embeddings2)
+    return cosine_scores[0][0]
+
+def process_files(origin_df, destination_df):
+    processed_pairs = set()
+    results = []
+
+    for _, row_origin in origin_df.iterrows():
+        origin_url = row_origin['url']  # Replace 'url' with the appropriate column name
+        for _, row_destination in destination_df.iterrows():
+            destination_url = row_destination['url']  # Replace 'url' with the appropriate column name
+            pair = (origin_url, destination_url)
+
+            if pair not in processed_pairs:
+                similarity_score = calculate_similarity(origin_url, destination_url)
+                results.append({'origin_url': origin_url, 'matched_url': destination_url, 'similarity_score': similarity_score})
+                processed_pairs.add(pair)
+
+    return pd.DataFrame(results)
+
+# Streamlit interface
+st.title("Automated Redirect Matching for Site Migrations")
 
 # File upload
-uploaded_origin = st.file_uploader("Upload origin.csv", type="csv")
-uploaded_destination = st.file_uploader("Upload destination.csv", type="csv")
+st.sidebar.title("Upload Files")
+origin_file = st.sidebar.file_uploader("Upload Origin URLs", type=['csv'])
+destination_file = st.sidebar.file_uploader("Upload Destination URLs", type=['csv'])
 
-# Function to process and match URLs based on similarity
-def process_and_match(origin_df, destination_df, selected_similarity_columns):
-    if not selected_similarity_columns:
-        raise ValueError("No columns selected for similarity matching.")
+if origin_file and destination_file:
+    origin_df = pd.read_csv(origin_file)
+    destination_df = pd.read_csv(destination_file)
 
-    # Validate that selected columns exist in both dataframes
-    valid_columns = [col for col in selected_similarity_columns if col in origin_df.columns and col in destination_df.columns]
-    if not valid_columns:
-        raise ValueError("Selected columns do not exist in both origin and destination dataframes.")
-
-    # Combine selected columns into a single text column for vectorization
-    origin_df['combined_text'] = origin_df[valid_columns].fillna('').astype(str).apply(' '.join, axis=1)
-    destination_df['combined_text'] = destination_df[valid_columns].fillna('').astype(str).apply(' '.join, axis=1)
-
-    # Use a pre-trained model for embedding
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-
-    # Vectorize the combined text
-    origin_embeddings = model.encode(origin_df['combined_text'].tolist(), show_progress_bar=True)
-    destination_embeddings = model.encode(destination_df['combined_text'].tolist(), show_progress_bar=True)
-
-    # Normalize embeddings to unit length for cosine similarity
-    origin_embeddings = origin_embeddings / np.linalg.norm(origin_embeddings, axis=1, keepdims=True)
-    destination_embeddings = destination_embeddings / np.linalg.norm(destination_embeddings, axis=1, keepdims=True)
-
-    # Create a FAISS index for Inner Product (cosine similarity)
-    dimension = origin_embeddings.shape[1]  # The dimension of vectors
-    faiss_index = faiss.IndexFlatIP(dimension)  # Inner Product for cosine similarity
-    faiss_index.add(destination_embeddings.astype('float32'))  # Add destination vectors to the index
-
-    # Perform the search for the nearest neighbors
-    D, I = faiss_index.search(origin_embeddings.astype('float32'), k=1)  # k=1 finds the closest match
-
-    # Convert distances to similarity scores (1 - distance for L2, direct output for IP)
-    similarity_scores = D.flatten()
-
-    # Create the output DataFrame
-    matches_df = pd.DataFrame({
-        'origin_url': origin_df['Address'],
-        'matched_url': destination_df['Address'].iloc[I.flatten()].values,
-        'similarity_score': np.round(similarity_scores, 4)  # Rounded for better readability
-    })
-
-    return matches_df
-
-# Function to find exact matches based on user-selected columns
-def find_exact_matches(origin_df, destination_df, exact_match_columns):
-    exact_matches_list = []
-    for col in exact_match_columns:
-        matched_rows = origin_df[origin_df[col].isin(destination_df[col])]
-        for _, row in matched_rows.iterrows():
-            exact_matches_list.append({
-                'origin_url': row['Address'],
-                'matched_url': destination_df[destination_df[col] == row[col]].iloc[0]['Address'],
-                'similarity_score': 1.0
-            })
-        origin_df = origin_df[~origin_df[col].isin(destination_df[col])]
-
-    exact_matches_df = pd.DataFrame(exact_matches_list)
-    return origin_df, exact_matches_df
-
-# Main function to control the flow
-def main():
-    if uploaded_origin is not None and uploaded_destination is not None:
-        # Load the dataframes
-        origin_df = pd.read_csv(uploaded_origin)
-        destination_df = pd.read_csv(uploaded_destination)
-
-        # Exact match column selection
-        st.write("Select columns for exact match pairing:")
-        exact_match_columns = st.multiselect('Exact Match Columns', options=list(origin_df.columns))
-
-        # Similarity match column selection
-        st.write("Select the columns you want to include for similarity matching:")
-        selected_similarity_columns = st.multiselect('Similarity Match Columns', options=list(origin_df.columns))
-
-        if st.button("Match URLs"):
-            try:
-                with st.spinner('Processing...'):
-                    # Clone the origin_df for similarity matching
-                    similarity_origin_df = origin_df.copy()
-
-                    # Find exact matches first
-                    origin_df, exact_matches_df = find_exact_matches(origin_df, destination_df, exact_match_columns)
-
-                    # Process and match URLs based on similarity for non-exact matches
-                    similarity_matches_df = process_and_match(similarity_origin_df, destination_df, selected_similarity_columns)
-
-                    # Combine exact and similarity-based matches
-                    final_matches_df = pd.concat([exact_matches_df, similarity_matches_df])
-
-                    st.write(final_matches_df)
-
-                    # Download link for the matches
-                    st.download_button(
-                        label="Download Matches as CSV",
-                        data=final_matches_df.to_csv(index=False).encode('utf-8'),
-                        file_name='matched_urls.csv',
-                        mime='text/csv',
-                    )
-            except ValueError as e:
-                st.error(f"Error: {e}")
-
-if __name__ == "__main__":
-    main()
+    if st.button("Run Matching"):
+        with st.spinner("Processing..."):
+            result_df = process_files(origin_df, destination_df)
+            st.write(result_df)
+            result_df.to_csv('matched_urls.csv', index=False)
+            st.sidebar.download_button(
+                label="Download Matched URLs as CSV",
+                data=result_df.to_csv().encode('utf-8'),
+                file_name='matched_urls.csv',
+                mime='text/csv',
+            )
